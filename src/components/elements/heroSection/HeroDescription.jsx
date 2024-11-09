d5be866a167f6dabd506b34c4c3c6d2046864011
Gifarfajar321 <gifarifajarm174@gmail.com>
2024-11-09 18:50:04 +0700

    menyesuaikan

--- src/components/elements/heroSection/HeroDescription.jsx
+++ src/components/elements/heroSection/HeroDescription.jsx
@@ -2,7 +2,7 @@ const HeroDescription = (props) => {
   const { children } = props;
   return (
     <>
-      <p className="font-inter font-light text-sm mb-2 text-dark md:text-base md:font-medium lg:text-sm xl:text-lg float-right">
+      <p className="font-inter font-light text-sm mb-5 text-dark md:text-base md:font-medium lg:text-sm xl:text-lg float-right">
         {children}
       </p>
     </>
