2c3784771453bf055454a7d591963df0253ecbde
FajarVerse <gifarifajarm174@gmail.com>
2024-11-10 13:14:52 +0700

    melakukan beberapa penyesuaian

--- src/components/elements/LinkBtn.jsx
+++ src/components/elements/LinkBtn.jsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
+import { DarkMode } from "../../assets/context/DarkModeContext";
+import { useContext } from "react";
 
 const LinkBtn = (props) => {
   const { link, className, children } = props;
+  const { darkMode } = useContext(DarkMode);
 
   return (
     <>
-      <Link to={link} target="_blank" className={`${className} btn-card `}>
+      <Link
+        to={link}
+        target="_blank"
+        className={`${className} ${
+          darkMode ? "btn-card-dark" : "btn-card-light"
+        } `}
+      >
         {children}
       </Link>
     </>
--- src/components/elements/SosmedLink.jsx
+++ src/components/elements/SosmedLink.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../assets/animasi/variants";
+import { DarkMode } from "../../assets/context/DarkModeContext";
+import { useContext } from "react";
 
 const SosmedLink = (props) => {
-  const { link, platform, username, children } = props;
+  const { link, platform, username, bgColor, children } = props;
+  const { darkMode } = useContext(DarkMode);
 
   return (
     <>
@@ -13,9 +16,20 @@ const SosmedLink = (props) => {
         whileInView={"show"}
         viewport={{ once: false, amount: 0.7 }}
       >
-        <Link to={`/${link}`} className="sosmed-icon group flex items-center">
+        <Link
+          to={`/${link}`}
+          className={`${
+            darkMode
+              ? "sosmed-icon-dark hover:ring-1 hover:ring-light"
+              : "sosmed-icon-light hover:ring-1 hover:ring-slate-500"
+          } group flex items-center hover:bg-[#${bgColor}]`}
+        >
           <span>{children}</span>
-          <span className="sosmed-username">{username}</span>
+          <span
+            className={`font-inter text-lg text-light leading-normal hidden lg:block lg:text-[0.8rem] xl:text-lg`}
+          >
+            {username}
+          </span>
         </Link>
       </motion.div>
     </>
