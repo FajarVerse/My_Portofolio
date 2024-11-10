e0930878ad2592210972568f2e521f480d358969
FajarVerse <gifarifajarm174@gmail.com>
2024-11-10 13:32:37 +0700

    melakukan perbaikan pada sosmed icon di section about

--- src/components/elements/SosmedLink.jsx
+++ src/components/elements/SosmedLink.jsx
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../assets/animasi/variants";
 import { DarkMode } from "../../assets/context/DarkModeContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const SosmedLink = (props) => {
-  const { link, platform, username, bgColor, children } = props;
+  const { link, username, bgColor, children } = props;
+  const [isHover, setIsHover] = useState(false);
   const { darkMode } = useContext(DarkMode);
 
   return (
@@ -22,7 +23,10 @@ const SosmedLink = (props) => {
             darkMode
               ? "sosmed-icon-dark hover:ring-1 hover:ring-light"
               : "sosmed-icon-light hover:ring-1 hover:ring-slate-500"
-          } group flex items-center hover:bg-[#${bgColor}]`}
+          } group flex items-center`}
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          style={{ backgroundColor: `${isHover ? bgColor : ""}` }}
         >
           <span>{children}</span>
           <span
